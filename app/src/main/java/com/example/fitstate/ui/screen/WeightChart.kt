d9eb7dfa74dd55cf6b53cb0b1326b95a57d8efaf
alirahimi <seyedalirahimi76@gmail.com>
2024-12-26 12:05:29 +0330

    [refactor] improve chart

--- app/src/main/java/com/example/fitstate/ui/screen/WeightChart.kt
+++ app/src/main/java/com/example/fitstate/ui/screen/WeightChart.kt
@@ -5,41 +5,127 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.fitstate.ui.model.BodyState
+import com.example.fitstate.ui.screen.component.rememberMarker
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
+import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLine
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
+import com.patrykandpatrick.vico.compose.cartesian.rememberVicoZoomState
+import com.patrykandpatrick.vico.compose.common.fill
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
+import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer
+import java.util.Calendar
 
 @Composable
 fun WeightChart(
-    modifier: Modifier = Modifier,
-    bodyStates: List<BodyState>,
+    modifier: Modifier = Modifier, bodyStates: List<BodyState>
 ) {
     if (bodyStates.isEmpty()) {
-        Text("No data available")
+        Text(text = "No data available", modifier = modifier)
     } else {
         val modelProducer = remember { CartesianChartModelProducer() }
         LaunchedEffect(bodyStates) {
             modelProducer.runTransaction {
                 lineSeries {
-                    series(bodyStates.map { it.weight.toDouble() })
+                    series(generateInterpolatedBodyStateList(bodyStates).map { it.weight.toDouble() })
                 }
             }
         }
+
+
         CartesianChartHost(
             chart = rememberCartesianChart(
-                rememberLineCartesianLayer(),
-                startAxis = VerticalAxis.rememberStart(),
-                bottomAxis = HorizontalAxis.rememberBottom(),
-            ),
+                layers = arrayOf(
+                    rememberLineCartesianLayer(
+                        LineCartesianLayer.LineProvider.series(
+                            LineCartesianLayer.rememberLine(remember {
+                                LineCartesianLayer.LineFill.single(
+                                    fill(Color(0xffa485e0))
+                                )
+                            })
+                        )
+                    )
+                ),
+                startAxis = VerticalAxis.rememberStart(
+                ),
+                bottomAxis = HorizontalAxis.rememberBottom(
+                    guideline = null,
+                    itemPlacer = remember { HorizontalAxis.ItemPlacer.segmented() },
+                    valueFormatter = bottomAxisValueFormatter
+                ),
+                marker = rememberMarker(),
+
+                ),
             modelProducer = modelProducer,
-            modifier = modifier
+            modifier = modifier,
+            zoomState = rememberVicoZoomState(zoomEnabled = false)
         )
     }
 }
+
+
+private val daysOfWeek = listOf("M", "T", "W", "T", "F", "S", "S")
+private val bottomAxisValueFormatter = CartesianValueFormatter { _, x, _ ->
+    daysOfWeek[x.toInt() % daysOfWeek.size]
+}
+
+
+fun generateInterpolatedBodyStateList(originalList: List<BodyState>): List<BodyState> {
+    if (originalList.isEmpty()) return emptyList()
+
+    val sortedList = originalList.sortedBy { it.date }
+
+    val calendar = Calendar.getInstance()
+    val interpolatedList = mutableListOf<BodyState>()
+
+    for (i in 0 until sortedList.size - 1) {
+        val current = sortedList[i]
+        val next = sortedList[i + 1]
+
+        val currentDate = current.date
+        val nextDate = next.date
+
+        val currentWeight = current.weight
+        val nextWeight = next.weight
+
+        interpolatedList.add(current)
+
+        calendar.time = currentDate
+        while (calendar.time.before(nextDate)) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+            val interpolatedDate = calendar.time
+
+            if (interpolatedDate.before(nextDate)) {
+                val daysBetween =
+                    ((nextDate.time - currentDate.time) / (1000 * 60 * 60 * 24)).toInt()
+                val dayIndex =
+                    ((interpolatedDate.time - currentDate.time) / (1000 * 60 * 60 * 24)).toInt()
+
+                // Calculate interpolated weight
+                val interpolatedWeight =
+                    currentWeight + (nextWeight - currentWeight) * (dayIndex.toFloat() / daysBetween)
+
+                interpolatedList.add(
+                    BodyState(
+                        weight = interpolatedWeight, date = interpolatedDate
+                    )
+                )
+            }
+        }
+    }
+
+    // Add the last BodyState
+    interpolatedList.add(sortedList.last())
+
+    return interpolatedList
+}
+
+
---
+++ app/src/main/java/com/example/fitstate/ui/screen/component/Marker.kt
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2024 by Patryk Goworowski and Patrick Michalik.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.fitstate.ui.screen.component
+
+import android.text.Layout
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
+import com.patrykandpatrick.vico.compose.cartesian.axis.rememberAxisGuidelineComponent
+import com.patrykandpatrick.vico.compose.common.component.fixed
+import com.patrykandpatrick.vico.compose.common.component.rememberLayeredComponent
+import com.patrykandpatrick.vico.compose.common.component.rememberShapeComponent
+import com.patrykandpatrick.vico.compose.common.component.rememberTextComponent
+import com.patrykandpatrick.vico.compose.common.component.shadow
+import com.patrykandpatrick.vico.compose.common.dimensions
+import com.patrykandpatrick.vico.compose.common.fill
+import com.patrykandpatrick.vico.compose.common.shape.markerCorneredShape
+import com.patrykandpatrick.vico.core.cartesian.CartesianMeasuringContext
+import com.patrykandpatrick.vico.core.cartesian.HorizontalDimensions
+import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModel
+import com.patrykandpatrick.vico.core.cartesian.marker.CartesianMarker
+import com.patrykandpatrick.vico.core.cartesian.marker.DefaultCartesianMarker
+import com.patrykandpatrick.vico.core.common.Fill
+import com.patrykandpatrick.vico.core.common.Insets
+import com.patrykandpatrick.vico.core.common.LayeredComponent
+import com.patrykandpatrick.vico.core.common.component.Shadow
+import com.patrykandpatrick.vico.core.common.component.ShapeComponent
+import com.patrykandpatrick.vico.core.common.component.TextComponent
+import com.patrykandpatrick.vico.core.common.shape.Corner
+import com.patrykandpatrick.vico.core.common.shape.CorneredShape
+
+@androidx.compose.runtime.Composable
+internal fun rememberMarker(
+  labelPosition: DefaultCartesianMarker.LabelPosition = DefaultCartesianMarker.LabelPosition.Top,
+  showIndicator: Boolean = true,
+): CartesianMarker {
+  val labelBackgroundShape =
+      markerCorneredShape(Corner.FullyRounded)
+  val labelBackground =
+      rememberShapeComponent(
+          fill = fill(MaterialTheme.colorScheme.surfaceBright),
+          shape = labelBackgroundShape,
+          shadow =
+          shadow(
+              radius = LABEL_BACKGROUND_SHADOW_RADIUS_DP.dp,
+              dy = LABEL_BACKGROUND_SHADOW_DY_DP.dp
+          ),
+      )
+  val label =
+      rememberTextComponent(
+          color = MaterialTheme.colorScheme.onSurface,
+          textAlignment = Layout.Alignment.ALIGN_CENTER,
+          padding = dimensions(8.dp, 4.dp),
+          background = labelBackground,
+          minWidth = TextComponent.MinWidth.fixed(40.dp),
+      )
+  val indicatorFrontComponent =
+      rememberShapeComponent(
+          fill(
+              color = MaterialTheme.colorScheme.onSurface,
+              ), CorneredShape.Companion.Pill
+      )
+  val indicatorCenterComponent =
+      rememberShapeComponent(shape = CorneredShape.Pill)
+  val indicatorRearComponent =
+      rememberShapeComponent(shape =CorneredShape.Pill)
+  val indicator =
+      rememberLayeredComponent(
+          rear = indicatorRearComponent,
+          front =
+          rememberLayeredComponent(
+              rear = indicatorCenterComponent,
+              front = indicatorFrontComponent,
+              padding = dimensions(5.dp),
+          ),
+          padding = dimensions(10.dp),
+      )
+  val guideline = rememberAxisGuidelineComponent()
+  return androidx.compose.runtime.remember(
+      label,
+      labelPosition,
+      indicator,
+      showIndicator,
+      guideline
+  ) {
+      object :
+          DefaultCartesianMarker(
+              label = label,
+              labelPosition = labelPosition,
+              indicator =
+              if (showIndicator) {
+                  { color ->
+                      LayeredComponent(
+                          rear =
+                          ShapeComponent(
+                              Fill(ColorUtils.setAlphaComponent(color, 38)),
+                              CorneredShape.Pill
+                          ),
+                          front =
+                          LayeredComponent(
+                              rear =
+                              ShapeComponent(
+                                  fill = Fill(color),
+                                  shape = CorneredShape.Pill,
+                                  shadow = Shadow(radiusDp = 12f, color = color),
+                              ),
+                              front = indicatorFrontComponent,
+                              padding = dimensions(5.dp),
+                          ),
+                          padding = dimensions(10.dp),
+                      )
+                  }
+              } else {
+                  null
+              },
+              indicatorSizeDp = 36f,
+              guideline = guideline,
+          ) {
+          override fun updateInsets(
+              context: CartesianMeasuringContext,
+              horizontalDimensions: HorizontalDimensions,
+              model: CartesianChartModel,
+              insets: Insets,
+          ) {
+              with(context) {
+                  val baseShadowInsetDp =
+                      CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER * LABEL_BACKGROUND_SHADOW_RADIUS_DP
+                  var topInset = (baseShadowInsetDp - LABEL_BACKGROUND_SHADOW_DY_DP).pixels
+                  var bottomInset = (baseShadowInsetDp + LABEL_BACKGROUND_SHADOW_DY_DP).pixels
+                  when (labelPosition) {
+                      LabelPosition.Top,
+                      LabelPosition.AbovePoint -> topInset += label.getHeight(context) + tickSizeDp.pixels
+
+                      LabelPosition.Bottom -> bottomInset += label.getHeight(context) + tickSizeDp.pixels
+                      LabelPosition.AroundPoint -> {}
+                  }
+                  insets.ensureValuesAtLeast(top = topInset, bottom = bottomInset)
+              }
+          }
+      }
+  }
+}
+
+private const val LABEL_BACKGROUND_SHADOW_RADIUS_DP = 4f
+private const val LABEL_BACKGROUND_SHADOW_DY_DP = 2f
+private const val CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER = 1.4f
